5d75d822f90e1d8bce1e12484c6005f5b12c659e
Jeff Mitsou <shinetachi88@gmail.com>
2021-11-28 23:49:17 -0500

    Project completed

--- src/main/java/com/careerdevs/placeholderV1/controllers/JSONController.java
+++ src/main/java/com/careerdevs/placeholderV1/controllers/JSONController.java
@@ -1,6 +1,7 @@
 package com.careerdevs.placeholderV1.controllers;
 
 import com.careerdevs.placeholderV1.models.Photos;
+import com.careerdevs.placeholderV1.models.Users;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
@@ -14,54 +15,78 @@ public class JSONController {
 
     //This class holds the methods that pull info with the mappings
 
-    /*@GetMapping ("/users") //GET all user data
-    public String allUsers (){
-        return String;
+    @GetMapping("/users/all") //GET all user data
+    public Users[] allUsers(RestTemplate restTemplate) {
+        String URL = "https://jsonplaceholder.typicode.com/users/";
+        return restTemplate.getForObject(URL, Users[].class);
     }
 
     @GetMapping ("/users/{ID}") //GET one user by ID
-    public String getUser (){
-        return String;
-    }
-
-    @GetMapping ("/") //Get all users within a range of IDs
-    public String selectUsers (){
-        return String;
-    }*/
-
-    // All the routes for the photos
-
-    @GetMapping ("/photos/all") //GET all photo data
-    public Photos[] getPhotos (RestTemplate restTemplate){
-        String URL = "https://jsonplaceholder.typicode.com/photos/";
-        return restTemplate.getForObject(URL, Photos[].class);
-    }
-
-    @GetMapping ("/photos/{ID}") //GET one photo by ID
-    public Object photoById (RestTemplate restTemplate, @PathVariable(name = "ID") String id){
-        String URL = "https://jsonplaceholder.typicode.com/photos/" + id;
+    public Object getUser (RestTemplate restTemplate, @PathVariable(name = "ID") String id ){
+        String URL = "https://jsonplaceholder.typicode.com/users/" + id;
 
         try {
 
-            return restTemplate.getForObject(URL, Photos.class);
+            return restTemplate.getForObject(URL, Users.class);
 
-        } catch (HttpClientErrorException.NotFound e){
+        } catch (HttpClientErrorException.NotFound e) {
 
-            return "Page not found";
+            return "User not found";
 
-        } catch (Exception e){
+        } catch (Exception e) {
 
             System.out.println(e.getMessage());
             return e.getMessage();
         }
     }
 
-    @GetMapping ("/photos") //Get all photos within a range of IDs
-    public Object selectPhotos (RestTemplate restTemplate, @RequestParam(name = "start") Integer start,
-                                                           @RequestParam(name = "end") Integer end){
+    @GetMapping ("/users") //Get all users within a range of IDs
+    public Object selectUsers (RestTemplate restTemplate, @RequestParam(name = "start") Integer start,
+                               @RequestParam(name = "end") Integer end){
+        ArrayList<Users> allUsers = new ArrayList<>();
+
+        for (int i = start; i <= end; i++) {
+            String URL = "https://jsonplaceholder.typicode.com/users/" + i;
+            Users onlyOne = restTemplate.getForObject(URL, Users.class);
+            Collections.addAll(allUsers, onlyOne);
+        }
+
+        return allUsers;
+    }
+
+    // All the routes for the photos
+
+    @GetMapping("/photos/all") //GET all photo data
+    public Photos[] getPhotos(RestTemplate restTemplate) {
+        String URL = "https://jsonplaceholder.typicode.com/photos/";
+        return restTemplate.getForObject(URL, Photos[].class);
+    }
+
+    @GetMapping("/photos/{ID}") //GET one photo by ID
+    public Object photoById(RestTemplate restTemplate, @PathVariable(name = "ID") String id) {
+        String URL = "https://jsonplaceholder.typicode.com/photos/" + id;
+
+        try {
+
+            return restTemplate.getForObject(URL, Photos.class);
+
+        } catch (HttpClientErrorException.NotFound e) {
+
+            return "Page not found";
+
+        } catch (Exception e) {
+
+            System.out.println(e.getMessage());
+            return e.getMessage();
+        }
+    }
+
+    @GetMapping("/photos") //Get all photos within a range of IDs
+    public Object selectPhotos(RestTemplate restTemplate, @RequestParam(name = "start") Integer start,
+                               @RequestParam(name = "end") Integer end) {
         ArrayList<Photos> allPhotos = new ArrayList<>();
 
-        for (int i = start; i <= end ; i++) {
+        for (int i = start; i <= end; i++) {
             String URL = "https://jsonplaceholder.typicode.com/photos/" + i;
             Photos single = restTemplate.getForObject(URL, Photos.class);
             Collections.addAll(allPhotos, single);
